4c11cef0df6585bb6c82528c6aa45b4157bdd434
joanord97 <joanord97@gmail.com>
2020-08-17 00:05:05 +0200

    made the general structure of the website

---
+++ Controllers/CVController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using portfolio.Models;
+
+namespace portfolio.Controllers
+{
+    public class CVController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
